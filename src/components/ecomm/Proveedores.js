965d895ee958e77f1a40458c7c1fd6117bed40e6
EriVillalta <evillaltadelacruz@gmail.com>
2017-09-09 17:21:29 -0500

    Formulario de Proveedores

--- src/components/ecomm/Proveedores.js
+++ src/components/ecomm/Proveedores.js
@@ -13,45 +13,98 @@ import TextField from 'material-ui/TextField';
 class Proveedores extends Component {
     render() {
         return (
-<Card>
-    <CardHeader
-        avatar={
-            <Avatar aria-label="Recipe" >
-                Add
-            </Avatar>
-    }
-        title="Agregar"
-        subheader="Productos"
-/>
-
-    <TextField
-        id="Nombre"
-        label="Proveedor"                        
-        
-        type="label"
+    <Card>
+        <CardHeader
+            avatar={
+                <Avatar aria-label="Recipe" >
+                    Add
+                </Avatar>
+        }
+            title="Agregar"
+            subheader="Productos"
     />
-    <br />
-    <br />
-    <TextField
-                    id="cantidad"
+
+                <TextField
+                id="Nombre"
+                label="Nombre"                        
+                    
+                type="text"
+                />
+                <br />
+                <br />
+                <TextField
+                    id="Apellidos"
+                    label="Apellidos"                        
+                    
+                    type="text"
+                 />
+                <br />
+                <br />
+    
+                <TextField
+                    id="text"
+                    label="Empresa"
+                />
+                <br />
+                <br />
+                <TextField
+                    id="number"
+                    label="Direccion"
+                    type="text"
+                />
+                <br />
+                <br />
+                <TextField
+                    id="number"
+                    label="Documento de Identidad"
+                    type="text"
+                />
+                <br />
+                <br />
+                <TextField
+                    id="text"
+                    label="Ruc"
+                    type="text"                        
+                />
+                <br />
+                <br />
+                <br />
+                <TextField
+                    id="date"
                     label=""
                     type="date"                        
                 />
                 <br />
+                <br />
+               
                 <TextField
-                    id="text"
-                    label="Empresa"
-                    label="Empresa"
-                    label="Empresa"
-                    label="Empresa"
-                    value={this.props.q}
-                    onChange={this.change}
-                    margin="normal"
+                    id="number"
+                    label="Celular"
+                    type="number"                        
                 />
                 <br />
-    </Card>
+                <br />
+                <TextField
+                    id="text"
+                    label="Email"
+                    type="email"                        
+                />
+                <br />
+                <br />
+
+
+                
+
+                <Button color="primary" aria-label="add">
+                <strong> Aceptar </strong>
+            </Button>    
+                <Button color="secondary" aria-label="add">
+                    <strong> Cancelar </strong>
+                </Button>    
+        </Card >
         );
     }
 }
+
 
 export default Proveedores;
