20c73c50dfbf7bc761706eee1840111fc491bf48
JOHN JACKSON ROCK <akzonrigg@gmail.com>
2017-09-09 13:45:53 -0700

    eliminando nav

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 //import PropTypes from 'prop-types'
 import { CardContent } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
-import './Header.css';
+// import './Header.css';
 import {
     Switch,
     Route,
@@ -24,17 +24,7 @@ class Header extends Component {
                 <Typography type="headline">
 				
 				
-                    <nav>
-						<ul style={{ listStyleType: 'none', padding: 0 }}>
-                            <li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
-                            <li><NavLink exact to="/abouts" activeClassName="selected">abouts</NavLink></li>
-                            <li><NavLink exact to="/one" activeClassName="selected">one</NavLink></li>
-                            <li><NavLink exact to="/counters" activeClassName="selected">addProduct</NavLink></li>
-                            <li><NavLink exact to="/users" activeClassName="selected">users</NavLink></li>
-                            <li><NavLink exact to="/ecomms" activeClassName="selected">ecomms</NavLink></li>
-                            <li><NavLink exact to="/form" activeClassName="selected">form</NavLink></li>
-                        </ul>
-					</nav>
+                    
 
                 </Typography>
 				
