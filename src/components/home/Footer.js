83bb88af0804ef9c312db8874af9408f8da31ff5
JOHN JACKSON ROCK <akzonrigg@gmail.com>
2017-09-09 22:09:17 -0700

    add text

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -13,14 +13,35 @@ class Footer extends Component {
                 "farmaceuticos cumpliendo una funcion de servicio"
                 <div>
                 <p class="catsandstars">
-                <br />and stars.
+                <br /> 
+                 Comparte Salud
+
+                 Comparte Salud es una asociación civil sin fines 
+                 de lucro que tiene como objetivo desarrollar programas asistenciales y obras sociales
+                 que beneficien a las personas que más lo necesitan. 
+                 Su misión es promover la ayuda social entre peruanos y canalizarla a los más necesitados y así, 
+                 compartir más salud entre ellos.
                  </p>
-                <p>This paragraph is not.</p>
-                <p class="catsandstars">
-                Here are more cats for you.<br />Look at them!
+                <p>Aquí encontrarás un espacio donde te daremos la mejor guía para ti, 
+                tus preparativos para la llegada de tu lindo bebé, el cuidado de tu pequeño 
+                en el transcurso de sus días y el bienestar y felicidad de toda tu familia.
+                Entérate de más ingresando aquí.
                 </p>
-              <p>And no more.</p>
+                <p class="catsandstars">
+                Súmate a nuestro equipo
+
+                <br />Nuestra misión es llevar con calidez y optimismo: salud, 
+                bienestar y ahorro a todas las comunidades del Perú. 
+                Esto es posible gracias a nuestros más de 11,000 colaboradores a nivel nacional, 
+                quienes día a día viven nuestros valores y nuestra visión de cambiar la historia 
+                de la salud en todas las comunidades donde operemos, a través de la mejor calidad, 
+                el mejor precio y la mejor gente. ¡Tú también sé parte de este gran equipo!
+                </p>
+              <p>leer.mas</p>
              </div>
+                </Typography>
+                <Typography>
+                <a href="http://inkafarma.bumeran.com.pe/index.bum" class="ver-mas" target="new">Leer más</a>
                 </Typography>
                 
             </CardContent>
